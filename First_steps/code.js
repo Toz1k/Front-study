4e4ae0c451306692346f2043ba3b6a99aa03b490
Toz1k <tozik1994@gmail.com>
2021-08-17 09:52:34 +0300

    New favicon added

--- First_steps/code.js
+++ First_steps/code.js
@@ -2,5 +2,23 @@
 
 
 // alert("No way, man");
+
+//alert("Hello, Alex!");
+
+/* document.addEventListener( 'click', () => {
+    window.location.href = "https://itc.ua";
+} )
+
+setTimeout( () => {
+    window.location.href = "https://itc.ua";
+}, 2000 )
+*/
+
+// alert(window.location.href = "https://itc.ua")
+
+
+
+
+
 console.log("Works fine for now");
 console.log("And connected to the git");
